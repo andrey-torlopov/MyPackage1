c20f66ba46bd179795087231b1342f16996862be
Andrey Torlopov <torlopov.mail@ya.ru>
2024-10-03 13:24:44 +0300

    init

--- Sources/MyPackage1/MyPackage1.swift
+++ Sources/MyPackage1/MyPackage1.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct MyPackage1 {
+    public var name: String = "MyPackage1"
+    
+    public init() { }
+    
+    public func demo1() -> String {
+        name
+    }
+}
