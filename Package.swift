9bb3662fc8e10853d579fbe6d1883242f6203882
Andrey Torlopov <torlopov.mail@ya.ru>
2024-10-03 16:50:23 +0300

    .

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "MyPackage1",
             targets: ["MyPackage1"]),
     ],
+    dependencies: [
+        .package(url: "git@github.com:andrey-torlopov/MyPackage2.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPackage1"),
+            name: "MyPackage1",
+            dependencies: ["MyPackage2"]
+        ),
         .testTarget(
             name: "MyPackage1Tests",
             dependencies: ["MyPackage1"]),
--- Sources/MyPackage1/MyPackage1.swift
+++ Sources/MyPackage1/MyPackage1.swift
@@ -1,12 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import MyPackage2
+
 public struct MyPackage1 {
     public var name: String = "MyPackage1_2"
     
     public init() { }
     
     public func demo1() -> String {
-        name
+        "\(name) \(Foo().foo())"
     }
 }
